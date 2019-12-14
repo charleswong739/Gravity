cc095d5121a4203a440c7cdd54dd51d141ef3b97
Charles <charleswonghm@gmail.com>
2019-12-14 02:49:35 -0500

    Added gravity between two objects

--- Assets/Scripts/AddForce.cs
+++ Assets/Scripts/AddForce.cs
@@ -6,21 +6,43 @@ public class AddForce : MonoBehaviour
 {
 
     [SerializeField]
-    private float mass = 20f;
+    private float mass = 1f;
 
-    [SerializeField]
-    private float force = 10f;
-    
-    private float v;
+    // [SerializeField]
+    // private float force = 10f;
+
+    private Vector3 force;
+    private Vector3 v;
+    private GameObject planet;
+    private Vector3 planetPos;
+    private float planetM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planet = GameObject.Find("TestPlanet");
+        planetPos = planet.GetComponent<Transform>().position;
+        planetM = 1E8f;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.C))
+        if (Input.GetKey(KeyCode.D))
         {
-            v += force / mass * Time.deltaTime;
+            force = 10 * Vector3.right;
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            force = -10 * Vector3.right;
+        } else 
+        {
+            force = Vector3.zero;
         }
 
-        transform.position += Vector3.right * v;
+        force += (planetPos - transform.position) * (Constants.G * planetM * mass / (planetPos - transform.position).sqrMagnitude);
+
+        v += force / mass * Time.deltaTime;
+
+        transform.position += v;
     }
 }
---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct Constants
+{
+    public const float G = 6.67E-11f;
+}
