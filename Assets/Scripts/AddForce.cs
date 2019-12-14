79dbfb7b5c6b62c40a24630ea37527123d2cfb4f
Charles <charleswonghm@gmail.com>
2019-12-13 21:43:34 -0500

    Script to add force and move object

---
+++ Assets/Scripts/AddForce.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+
+    [SerializeField]
+    private float mass = 20f;
+
+    [SerializeField]
+    private float force = 10f;
+    
+    private float v;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            v += force / mass * Time.deltaTime;
+        }
+
+        transform.position += Vector3.right * v;
+    }
+}
